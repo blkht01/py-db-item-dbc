11823a3e5f6b9926f9778686d1852634a6867452
blkht01 <25400057+blkht01@users.noreply.github.com>
2024-04-01 01:12:35 +0200

    Initial commit

---
+++ py-db-to-item-dbc.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2024, blkht01@github
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. 
+
+import pymysql
+
+# Database connection parameters
+db_config = {
+    'host': 'localhost',        # IP of your database server
+    'user': 'acore',            # Database user
+    'password': 'acore',        # Database password
+    'database': 'acore_world',  # Database name
+    'charset': 'utf8mb4',       # Database charset (probably leave this as is)
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+# Define the range for the entries you want to extract
+entry_start = 1
+entry_end = 999999
+
+# SQL query to fetch data from item_template with a range filter
+select_query = f"""
+SELECT 
+    entry as ID, 
+    class as ClassID, 
+    SoundOverrideSubclass as Sound_Override_subclassid, 
+    Material, 
+    displayid as DisplayInfoID, 
+    InventoryType, 
+    sheath as SheatheType 
+FROM 
+    item_template
+WHERE 
+    entry BETWEEN {entry_start} AND {entry_end}
+"""
+
+# Connect to the database
+connection = pymysql.connect(**db_config)
+
+try:
+    with connection.cursor() as cursor:
+        # Execute the select query
+        cursor.execute(select_query)
+        
+        # Fetch all the rows
+        rows = cursor.fetchall()
+        
+        # Prepare the SQL file content
+        sql_inserts = []
+        for row in rows:
+            sql_insert = f"INSERT INTO dbc.db_item_12340 (ID, ClassID, Sound_Override_subclassid, Material, DisplayInfoID, InventoryType, SheatheType) VALUES ({row['ID']}, {row['ClassID']}, {row['Sound_Override_subclassid']}, {row['Material']}, {row['DisplayInfoID']}, {row['InventoryType']}, {row['SheatheType']});"
+            sql_inserts.append(sql_insert)
+        
+        # Save to a SQL file
+        with open('inserts_into_db_item_12340.sql', 'w') as file:
+            file.write('\n'.join(sql_inserts))
+        
+        print("SQL file generated successfully.")
+
+finally:
+    # Close the connection
+    connection.close()
